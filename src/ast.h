264749dcf10fb3e89e0a38760fabd31739c2055f
JakubBakaj <jakub.bakaj3@gmail.com>
2025-10-18 16:14:42 +0200

    add NodeType Definiton

---
+++ src/ast.h
@@ -0,0 +1,23 @@
+#ifndef AST_H
+#define AST_H
+
+typedef enum{
+    ND_FN_DECL,
+    ND_VAR_DECL,
+    ND_CONST_DECL,
+    ND_BLOCK_STMT,
+    ND_EXPR_STMT,
+    ND_RETURN_STMT,
+    ND_BINARY_EXPR,
+    ND_UNARY_EXPR,
+    ND_INT_LITERAL,
+    ND_STRING_LITERAL,
+    ND_IDENT_EXPR,
+    ND_CALL_EXPR,
+} NodeType;
+
+typedef struct {
+
+} Node;
+
+#endif // AST_H
